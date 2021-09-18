92e4edb62ff034c00c38fa60bb85f69e36e2d7d4
makarenkovdo <makarenkovdo@gmail.com>
2021-09-18 14:20:40 +0300

    cleaning

--- src/uiKits/formElements/formElements.js
+++ src/uiKits/formElements/formElements.js
@@ -12,29 +12,18 @@ function handleClick() {
     console.log(isOpen)
     isOpen ? closeDropdownBox() : openDropdownBox()
     function closeDropdownBox() {
-        // const $dropdownBox = document.querySelector('dropdown-box')
-        // console.log($dropdownBox)
-        // $dropdownBoxContainer.innerHTML = ''
-        // console.log('CLOSE!!')
         $dropdownBoxContainer.classList.add('hide')
 
         isOpen = false
     }
     function openDropdownBox() {
-        // const dropdownBox = document.createElement('div')
-        // dropdownBox.classList.add('dropdown-box')
-        // // $dropdownContainer.insertAdjacentElement('beforeend', <div>asd</div>)
-        // console.log('CLICK!')
         const x = $dropdownContainer.getBoundingClientRect().x
         const y =
             $dropdownContainer.getBoundingClientRect().y +
             $dropdownContainer.getBoundingClientRect().height
         console.log(x, y)
         $dropdownBoxContainer.classList.remove('hide')
-        // $dropdownBoxContainer.style.left = x
-        // $dropdownBoxContainer.style.top = y
         $dropdownBoxContainer.style.cssText = `left:${x}px; top: ${y}px; `
-        // $dropdownBoxContainer.insertAdjacentElement('beforeend', dropdownBox)
 
         isOpen = true
     }
