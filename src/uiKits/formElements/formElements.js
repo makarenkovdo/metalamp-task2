a25cbfdde49c8f6f4a6db5070e20f57a87846fca
makarenkovdo <makarenkovdo@gmail.com>
2021-09-18 14:20:05 +0300

    redone open/close with add and remove 'hide' class

--- src/uiKits/formElements/formElements.js
+++ src/uiKits/formElements/formElements.js
@@ -4,35 +4,37 @@ import someSvg from './../../components/dropdown-guests/dropdown-guests.svg'
 let isOpen = false
 const $dropdownContainerImg = document.querySelector('.js-img-box')
 const $dropdownContainer = document.querySelector('.js-dropdown-guests')
-const $dropdownBoxContainer = document.querySelector(
-    '.js-dropdown-box-container'
-)
+const $dropdownBoxContainer = document.querySelector('.js-dropdown-box')
+
 $dropdownContainerImg.addEventListener('click', handleClick)
 
 function handleClick() {
     console.log(isOpen)
     isOpen ? closeDropdownBox() : openDropdownBox()
     function closeDropdownBox() {
-        const $dropdownBox = document.querySelector('dropdown-box')
-        console.log($dropdownBox)
-        $dropdownBoxContainer.innerHTML = ''
-        console.log('CLOSE!!')
+        // const $dropdownBox = document.querySelector('dropdown-box')
+        // console.log($dropdownBox)
+        // $dropdownBoxContainer.innerHTML = ''
+        // console.log('CLOSE!!')
+        $dropdownBoxContainer.classList.add('hide')
+
         isOpen = false
     }
     function openDropdownBox() {
-        const dropdownBox = document.createElement('div')
-        dropdownBox.classList.add('dropdown-box')
-        // $dropdownContainer.insertAdjacentElement('beforeend', <div>asd</div>)
-        console.log('CLICK!')
+        // const dropdownBox = document.createElement('div')
+        // dropdownBox.classList.add('dropdown-box')
+        // // $dropdownContainer.insertAdjacentElement('beforeend', <div>asd</div>)
+        // console.log('CLICK!')
         const x = $dropdownContainer.getBoundingClientRect().x
         const y =
             $dropdownContainer.getBoundingClientRect().y +
             $dropdownContainer.getBoundingClientRect().height
         console.log(x, y)
-        // dropdownBox.style.left = x
-        // dropdownBox.style.top = y
-        dropdownBox.style.cssText = `left:${x}px; top: ${y}px; `
-        $dropdownBoxContainer.insertAdjacentElement('beforeend', dropdownBox)
+        $dropdownBoxContainer.classList.remove('hide')
+        // $dropdownBoxContainer.style.left = x
+        // $dropdownBoxContainer.style.top = y
+        $dropdownBoxContainer.style.cssText = `left:${x}px; top: ${y}px; `
+        // $dropdownBoxContainer.insertAdjacentElement('beforeend', dropdownBox)
 
         isOpen = true
     }
