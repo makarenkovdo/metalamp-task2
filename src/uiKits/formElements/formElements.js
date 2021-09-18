f169a8e19b242b5dddac8ac0f092b5241290bd75
makarenkovdo <makarenkovdo@gmail.com>
2021-09-18 11:16:44 +0300

    implement open/close

--- src/uiKits/formElements/formElements.js
+++ src/uiKits/formElements/formElements.js
@@ -1,15 +1,30 @@
 import './formElements.scss'
 import someSvg from './../../components/dropdown-guests/dropdown-guests.svg'
 
+let isOpen = false
 const $dropdownContainerImg = document.querySelector('.js-img-box')
 const $dropdownContainer = document.querySelector('.js-dropdown-guests')
+const $dropdownBoxContainer = document.querySelector(
+    '.js-dropdown-box-container'
+)
+$dropdownContainerImg.addEventListener('click', handleClick)
 
-$dropdownContainerImg.addEventListener('click', createDropdownBox)
-
-function createDropdownBox() {
-    const dropdownBox = document.createElement('div')
-    dropdownBox.classList.add('dropdown-box')
-    // $dropdownContainer.insertAdjacentElement('beforeend', <div>asd</div>)
-    console.log('CLICK!')
-    $dropdownContainer.insertAdjacentElement('beforeend', dropdownBox)
+function handleClick() {
+    console.log(isOpen)
+    isOpen ? closeDropdownBox() : openDropdownBox()
+    function closeDropdownBox() {
+        const $dropdownBox = document.querySelector('dropdown-box')
+        console.log($dropdownBox)
+        $dropdownBoxContainer.innerHTML = ''
+        console.log('CLOSE!!')
+        isOpen = false
+    }
+    function openDropdownBox() {
+        const dropdownBox = document.createElement('div')
+        dropdownBox.classList.add('dropdown-box')
+        // $dropdownContainer.insertAdjacentElement('beforeend', <div>asd</div>)
+        console.log('CLICK!')
+        $dropdownBoxContainer.insertAdjacentElement('beforeend', dropdownBox)
+        isOpen = true
+    }
 }
