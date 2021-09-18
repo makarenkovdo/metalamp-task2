8b2924cce0e14c6d67d32ce2744750b70b0648ef
makarenkovdo <makarenkovdo@gmail.com>
2021-09-18 09:27:13 +0300

    js - click on image - console.log

--- src/uiKits/formElements/formElements.js
+++ src/uiKits/formElements/formElements.js
@@ -2,3 +2,11 @@ import './formElements.scss'
 import someSvg from './../../components/dropdown-guests/dropdown-guests.svg'
 
 console.log('formElements here')
+const $dropdownBoxImg = document.querySelector('.js-img-box')
+$dropdownBoxImg.addEventListener('click', dropdownBoxCreate)
+
+function dropdownBoxCreate() {
+    const $dropdownContainer = document.querySelector('.js-dropdown-guests')
+    // $dropdownContainer.insertAdjacentElement('beforeend', <div>asd</div>)
+    console.log('CLICK!')
+}
