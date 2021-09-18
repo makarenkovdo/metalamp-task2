74ae43e8b998e3cf0c94698fdeda5b244876ee0b
makarenkovdo <makarenkovdo@gmail.com>
2021-09-18 10:30:19 +0300

    creating dropdownBox by click

--- src/uiKits/formElements/formElements.js
+++ src/uiKits/formElements/formElements.js
@@ -1,12 +1,15 @@
 import './formElements.scss'
 import someSvg from './../../components/dropdown-guests/dropdown-guests.svg'
 
-console.log('formElements here')
-const $dropdownBoxImg = document.querySelector('.js-img-box')
-$dropdownBoxImg.addEventListener('click', dropdownBoxCreate)
+const $dropdownContainerImg = document.querySelector('.js-img-box')
+const $dropdownContainer = document.querySelector('.js-dropdown-guests')
 
-function dropdownBoxCreate() {
-    const $dropdownContainer = document.querySelector('.js-dropdown-guests')
+$dropdownContainerImg.addEventListener('click', createDropdownBox)
+
+function createDropdownBox() {
+    const dropdownBox = document.createElement('div')
+    dropdownBox.classList.add('dropdown-box')
     // $dropdownContainer.insertAdjacentElement('beforeend', <div>asd</div>)
     console.log('CLICK!')
+    $dropdownContainer.insertAdjacentElement('beforeend', dropdownBox)
 }
