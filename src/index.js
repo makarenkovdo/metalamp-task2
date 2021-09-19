782d1ff11a9bd1ad3c9a15b89e741554d41158e4
makarenkovdo <makarenkovdo@gmail.com>
2021-09-19 18:32:45 +0300

    2 inputs datepicker

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 import 'jquery'
 import 'air-datepicker'
 import './../node_modules/air-datepicker/dist/css/datepicker.css'
-
-$('#minMaxExample').datepicker({
-    // Можно выбрать тольо даты, идущие за сегодняшним днем, включая сегодня
-    minDate: new Date()
-})
-
-$(function () {
-    $('#calendar').datepicker({range: true})
-})
+import './pages/datepicker'
--- src/pages/datepicker.js
+++ src/pages/datepicker.js
@@ -1,8 +1,15 @@
-import 'jquery'
-import 'air-datepicker'
-import './../uikits/formElements/formElements'
-import './../../node_modules/air-datepicker/src/sass/_datepicker-config.scss'
-import './../../node_modules/air-datepicker/src/sass/cell.scss'
-import './../../node_modules/air-datepicker/src/sass/datepicker.scss'
-import './../../node_modules/air-datepicker/src/sass/navigation.scss'
-import './../../node_modules/air-datepicker/src/sass/timepicker.scss'
+$('#start_one').datepicker({
+    onSelect: function (fd, d, picker) {
+        $('#start_one').val(fd.split('-')[0])
+        $('#end_one').val(fd.split('-')[1])
+    },
+})
+
+$('#minMaxExample').datepicker({
+    // Можно выбрать тольо даты, идущие за сегодняшним днем, включая сегодня
+    minDate: new Date(),
+})
+
+$(function () {
+    $('#calendar').datepicker({ range: true })
+})
