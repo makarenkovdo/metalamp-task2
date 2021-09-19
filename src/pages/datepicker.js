e2dafc2583c5d4a49f37fc8ee565f64c2036fe0c
makarenkovdo <makarenkovdo@gmail.com>
2021-09-19 19:02:11 +0300

    correct selections

--- src/pages/datepicker.js
+++ src/pages/datepicker.js
@@ -4,12 +4,34 @@ $('#start_one').datepicker({
         $('#end_one').val(fd.split('-')[1])
     },
 })
+$('#end_one').datepicker({
+    onSelect: function (fd, d, picker) {
+        $('#start_one').val(fd.split('-')[0])
+        $('#end_one').val(fd.split('-')[1])
+    },
+})
+
+// var $start = $('#start'),
+//     $end = $('#end')
+
+// $start.datepicker({
+//     onSelect: function (fd, date) {
+//         $end.data('datepicker').update('minDate', date)
+
+//         $end.focus()
+//     },
+// })
+// $end.datepicker({
+//     onSelect: function (fd, date) {
+//         $start.data('datepicker').update('maxDate', date)
+//     },
+// })
 
 $('#minMaxExample').datepicker({
     // Можно выбрать тольо даты, идущие за сегодняшним днем, включая сегодня
     minDate: new Date(),
 })
 
-$(function () {
-    $('#calendar').datepicker({ range: true })
-})
+// $(function () {
+//     $('#calendar').datepicker({ range: true })
+// })
