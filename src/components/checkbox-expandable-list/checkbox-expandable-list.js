8d79f8527b31874e16a30c6055874f5ae26d0295
makarenkovdo <makarenkovdo@gmail.com>
2021-10-03 08:30:06 +0300

    exp-ch-list: refactoring to 'details' tag

--- src/components/checkbox-expandable-list/checkbox-expandable-list.js
+++ src/components/checkbox-expandable-list/checkbox-expandable-list.js
@@ -1,38 +1,38 @@
-const $checkboxListContainer = document.querySelector(
-    '.js-checkbox-expandable-list'
-)
-const $checkboxListPopup = document.querySelector(
-    '.js-checkbox-expandable-list-popup'
-)
-const $checkboxListText = document.querySelector(
-    '.js-checkbox-expandable-list-text'
-)
-const $checkboxListIconSelector = document.querySelector(
-    'js-checkbox-expandable-list-icon'
-)
-const $checkboxListIconBox = document.querySelector(
-    '.js-checkbox-expandable-list-icon-box'
-)
+// const $checkboxListContainer = document.querySelector(
+//     '.js-checkbox-expandable-list'
+// )
+// const $checkboxListPopup = document.querySelector(
+//     '.js-checkbox-expandable-list-popup'
+// )
+// const $checkboxListText = document.querySelector(
+//     '.js-checkbox-expandable-list-text'
+// )
+// const $checkboxListIconSelector = document.querySelector(
+//     'js-checkbox-expandable-list-icon'
+// )
+// const $checkboxListIconBox = document.querySelector(
+//     '.js-checkbox-expandable-list-icon-box'
+// )
 
-$checkboxListIconBox.addEventListener('click', handleCheckboxListClick)
+// $checkboxListIconBox.addEventListener('click', handleCheckboxListClick)
 
-function handleCheckboxListClick() {
-    $checkboxListPopup.classList.contains('hide')
-        ? openCheckboxList()
-        : closeCheckboxList()
-    function closeCheckboxList() {
-        $checkboxListPopup.classList.add('hide')
-        $checkboxListIconSelector.src =
-            './img/close-checkbox-expandable-list.svg'
-    }
-    function openCheckboxList() {
-        const x = $checkboxListContainer.getBoundingClientRect().x
-        const y =
-            $checkboxListContainer.getBoundingClientRect().y +
-            $checkboxListContainer.getBoundingClientRect().height
-        $checkboxListPopup.classList.remove('hide')
-        $checkboxListPopup.style.cssText = `left:${x}px; top: ${y}px; `
-        $checkboxListIconSelector.src =
-            './img/open-checkbox-expandable-list.svg'
-    }
-}
+// function handleCheckboxListClick() {
+//     $checkboxListPopup.classList.contains('hide')
+//         ? openCheckboxList()
+//         : closeCheckboxList()
+//     function closeCheckboxList() {
+//         $checkboxListPopup.classList.add('hide')
+//         $checkboxListIconSelector.src =
+//             './img/close-checkbox-expandable-list.svg'
+//     }
+//     function openCheckboxList() {
+//         const x = $checkboxListContainer.getBoundingClientRect().x
+//         const y =
+//             $checkboxListContainer.getBoundingClientRect().y +
+//             $checkboxListContainer.getBoundingClientRect().height
+//         $checkboxListPopup.classList.remove('hide')
+//         $checkboxListPopup.style.cssText = `left:${x}px; top: ${y}px; `
+//         $checkboxListIconSelector.src =
+//             './img/open-checkbox-expandable-list.svg'
+//     }
+// }
