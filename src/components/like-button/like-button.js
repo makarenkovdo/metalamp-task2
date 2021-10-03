69cd0c3f9aa7e27216bec582bfd24ada49938b82
makarenkovdo <makarenkovdo@gmail.com>
2021-10-03 16:15:26 +0300

    like button js refactoring (not at all), ratebut

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,32 +1,43 @@
 console.log('something')
-const $likeButtonSelector = document.querySelector('.js-like-button')
+
+const $likeButtonSelector = document.querySelector(`.js-like-button`)
 const $likeIconSelector = document.querySelector('.js-like-button__icon')
 const $likeButtonCounter = document.querySelector('.js-like-button__counter')
-$likeButtonSelector.addEventListener('click', () =>
-    handleLikeButtonClick(twoLikesButton)
-)
+
+// $likeButtonSelector.addEventListener('click', () =>
+//     handleLikeButtonClick(twoLikesButton)
+// )
+
+par = document.querySelector('.form-elements__like-button-container')
+par.addEventListener('click', function (e) {
+    handleLikeButtonClick(twoLikesButton, e)
+})
+
 function likeButtonCounter(n) {
     let counter = n
     return function changeLikeButtonCounter(shift) {
         counter += shift
-        $likeButtonCounter.innerHTML = counter
+        e.target.innerHTML = counter
     }
 }
 const twoLikesButton = likeButtonCounter(2)
 const elevenLikesButton = likeButtonCounter(11)
 
-function handleLikeButtonClick(currentButton) {
-    console.log('CLICK')
-    if ($likeButtonSelector.classList.contains('like-button_active')) {
-        $likeButtonSelector.classList.remove('like-button_active')
-        $likeButtonCounter.classList.remove('like-button__counter-active')
-        $likeIconSelector.src = './img/like-border.svg'
+function handleLikeButtonClick(currentButton, e) {
+    console.log(e.target.parentElement)
+
+    if (e.target.classList.contains('like-button_active')) {
+        e.target.classList.remove('like-button_active')
+        e.target.classList.remove('like-button__counter-active')
+        e.currentTarget.querySelector('.js-like-button__icon').src =
+            './img/like-border.svg'
         currentButton(-1)
     } else {
-        console.log('ADD CLASS')
-        $likeButtonSelector.classList.add('like-button_active')
-        $likeButtonCounter.classList.add('like-button__counter-active')
-        $likeIconSelector.src = './img/like-active.svg'
+        e.target.classList.add('like-button_active')
+        e.target.classList.add('like-button__counter-active')
+
+        e.currentTarget.querySelector('.js-like-button__icon').src =
+            './img/like-active.svg'
         currentButton(+1)
     }
 }
