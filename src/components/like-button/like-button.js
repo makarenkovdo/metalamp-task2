664a038d7486a2f6adbfbe066a2a2e868933b3ea
makarenkovdo <makarenkovdo@gmail.com>
2021-10-04 08:12:31 +0300

    like-button js-logic

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,44 +1,56 @@
-console.log('something')
-
-const $likeButtonSelector = document.querySelector(`.js-like-button`)
-const $likeIconSelector = document.querySelector('.js-like-button__icon')
-const $likeButtonCounter = document.querySelector('.js-like-button__counter')
+let $quantityArray = $(`.like-button`)
+const $likeButtonSelector = []
+const $likeIconSelector = []
+const $likeButtonCounter = []
+for (let i = 1; i <= $quantityArray.length; i++) {
+    $likeButtonSelector[i] = document.querySelector(
+        `.js-like-button_instance-${i}`
+    )
+    $likeIconSelector[i] = document.querySelector(
+        `.js-like-button__icon_instance-${i}`
+    )
+    $likeButtonCounter[i] = document.querySelector(
+        `.js-like-button__counter_instance-${i}`
+    )
+    $likeButtonSelector[i].addEventListener('click', function (e) {
+        handleLikeButtonClick(likeButtonCounter, e)
+    })
+}
 
 // $likeButtonSelector.addEventListener('click', () =>
 //     handleLikeButtonClick(twoLikesButton)
 // )
 
-par = document.querySelector('.form-elements__like-button-container')
-par.addEventListener('click', function (e) {
-    handleLikeButtonClick(twoLikesButton, e)
-})
-
-function likeButtonCounter(n) {
-    let counter = n
-    return function changeLikeButtonCounter(shift) {
-        counter += shift
-        e.target.innerHTML = counter
-    }
+function likeButtonCounter(shift, currentId) {
+    let counter = +$likeButtonCounter[currentId].innerHTML
+    counter += shift
+    $likeButtonCounter[currentId].innerHTML = counter
 }
-const twoLikesButton = likeButtonCounter(2)
-const elevenLikesButton = likeButtonCounter(11)
+// const twoLikesButton = likeButtonCounter(2)
+// const elevenLikesButton = likeButtonCounter(11)
 
 function handleLikeButtonClick(currentButton, e) {
-    console.log(e.target.parentElement)
+    let currentId = e.target.id
+    console.log(currentId)
+    console.log($likeButtonSelector[currentId])
 
-    if (e.target.classList.contains('like-button_active')) {
-        e.target.classList.remove('like-button_active')
-        e.target.classList.remove('like-button__counter-active')
-        e.currentTarget.querySelector('.js-like-button__icon').src =
-            './img/like-border.svg'
-        currentButton(-1)
+    if (
+        $likeButtonSelector[currentId].classList.contains('like-button_active')
+    ) {
+        console.log('here')
+        $likeButtonSelector[currentId].classList.remove('like-button_active')
+        $likeButtonCounter[currentId].classList.remove(
+            'like-button__counter-active'
+        )
+        $likeIconSelector[currentId].src = './img/like-border.svg'
+        currentButton(-1, currentId)
     } else {
-        e.target.classList.add('like-button_active')
-        e.target.classList.add('like-button__counter-active')
-
-        e.currentTarget.querySelector('.js-like-button__icon').src =
-            './img/like-active.svg'
-        currentButton(+1)
+        $likeButtonSelector[currentId].classList.add('like-button_active')
+        $likeButtonCounter[currentId].classList.add(
+            'like-button__counter-active'
+        )
+        $likeIconSelector[currentId].src = './img/like-active.svg'
+        currentButton(+1, currentId)
     }
 }
 // $(document).ready(function () {
