86f7ae4c3c91ba9d9b579eb060389ccdd2cff887
makarenkovdo <makarenkovdo@gmail.com>
2021-10-06 07:35:14 +0300

    add slick-folder, datepicker-refactor

--- src/components/datepicker/datepicker.js
+++ src/components/datepicker/datepicker.js
@@ -3,18 +3,50 @@ import './../../vendor/air-datepicker/scss/cell.scss'
 import './../../vendor/air-datepicker/scss/navigation.scss'
 import './../../vendor/air-datepicker/js/air-datepicker.js'
 
-$('#start_one').datepicker({
+// //second version
+// $('.js-datepicker-input').datepicker({
+//     onSelect: function (fd, d, picker) {
+//         $('#start_one').val(fd.split('-')[0])
+//         $('#end_one').val(fd.split('-')[1])
+//     },
+// })
+
+// good!
+// $('#start_one').click(function (e) {
+//     e.preventDefault()
+//     var par = $(event.target).parent().parent()
+//     // par.find('#start_one').css('background', 'yellow')
+//     // $('.js-datepicker-input_second').css('background', 'yellow')
+//     // $('.datepicker-here').prev().focus()
+// })
+
+$('.datepicker-here').datepicker({
     onSelect: function (fd, d, picker) {
-        $('#start_one').val(fd.split('-')[0])
-        $('#end_one').val(fd.split('-')[1])
+        $('.datepicker-here').val(fd.split('-')[0])
+        $('.js-datepicker-input_second').val(fd.split('-')[1])
     },
 })
-$('#end_one').datepicker({
-    onSelect: function (fd, d, picker) {
-        $('#start_one').val(fd.split('-')[0])
-        $('#end_one').val(fd.split('-')[1])
-    },
+
+// $("button").click(function(){
+//     var closest_ones = $(this).prev("button").add($(this).next("button"));
+//     closest_ones.text(++counter);
+// });
+
+$('.js-datepicker-input_second').click(function (e) {
+    e.preventDefault()
+    var par = $(event.target).parent().parent()
+    par.find('#start_one').focus()
+    // $('.js-datepicker-input_second').css('background', 'yellow')
+    // $('.datepicker-here').prev().focus()
 })
+
+//OLD OLD VERSIONS
+// $('#end_one').datepicker({
+//     onSelect: function (fd, d, picker) {
+//         $('#start_one').val(fd.split('-')[0])
+//         $('#end_one').val(fd.split('-')[1])
+//     },
+// })
 
 // var $start = $('#start'),
 //     $end = $('#end')
