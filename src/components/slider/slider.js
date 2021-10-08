2aa3c992270af5199055bcc4ee60dbf0f0e9b8d5
makarenkovdo <makarenkovdo@gmail.com>
2021-10-08 19:44:45 +0300

    installed multiply sliders

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -14,14 +14,20 @@ import jquery from 'jquery'
 import '../../../node_modules/@splidejs/splide/dist/css/splide.min.css'
 import Splide from '@splidejs/splide'
 
-new Splide('.splide', {
-    classes: {
-        arrows: 'splide__arrows splide__new-arrows',
-        arrow: 'splide__arrow splide__new-arrow',
-        prev: 'splide__arrow--prev splide__new-arrows_prev',
-        next: 'splide__arrow--next splide__new-arrows_next',
-    },
-}).mount()
+// new Splide('.splide', {
+//     classes: {
+//         arrows: 'splide__arrows splide__new-arrows',
+//         arrow: 'splide__arrow splide__new-arrow',
+//         prev: 'splide__arrow--prev splide__new-arrows_prev',
+//         next: 'splide__arrow--next splide__new-arrows_next',
+//     },
+// }).mount()
+
+var elms = document.getElementsByClassName('splide')
+
+for (var i = 0; i < elms.length; i++) {
+    new Splide(elms[i]).mount()
+}
 
 /* <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M0.578125 1.40625L1.98438 2.38311e-07L7.98438 6L1.98438 12L0.578125 10.5938L5.17188 6L0.578125 1.40625Z" fill="white"/>
