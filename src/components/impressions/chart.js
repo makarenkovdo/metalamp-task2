b2e6c54fc34d7f88c7e2438f6f2df1e1eab2a5b2
makarenkovdo <makarenkovdo@gmail.com>
2021-10-11 20:29:07 +0300

    try to configure chartjs

--- src/components/impressions/chart.js
+++ src/components/impressions/chart.js
@@ -1,39 +1,24 @@
 import { Chart, registerables } from 'chart.js'
 Chart.register(...registerables)
 var ctx = document.getElementById('myChart').getContext('2d')
-var myChart = new Chart(ctx, {
-    type: 'bar',
+
+new Chart(document.getElementById('myChart'), {
+    type: 'doughnut',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Великолепно', 'Хорошо', 'Удовлетворен', 'Разочарован'],
         datasets: [
             {
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                ],
-                borderWidth: 1,
+                label: 'Впечатления от номера)',
+                backgroundColor: ['#C4C4C4', '#C4C4C4', '#3cba9f', '#e8c3b9'],
+                data: [50, 25, 25, 0],
             },
         ],
     },
     options: {
-        scales: {
-            y: {
-                beginAtZero: true,
-            },
+        title: {
+            display: true,
+            text: 'Predicted world population (millions) in 2050',
         },
+        cutoutPercentage: 10,
     },
 })
