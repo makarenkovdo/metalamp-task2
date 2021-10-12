1123cc82d414affaf524f7f5bd0a57ba5cd7979f
makarenkovdo <makarenkovdo@gmail.com>
2021-10-12 11:53:45 +0300

    adding gradient

--- src/components/impressions/chart.js
+++ src/components/impressions/chart.js
@@ -2,6 +2,16 @@ import { Chart, registerables } from 'chart.js'
 Chart.register(...registerables)
 var ctx = document.getElementById('myChart').getContext('2d')
 
+var gradientA = ctx.createLinearGradient(0, 0, 0, 600)
+gradientA.addColorStop(0, 'rgb(255, 186, 156)')
+gradientA.addColorStop(1, 'rgb(255, 228, 156)')
+var gradientB = ctx.createLinearGradient(0, 0, 0, 600)
+gradientB.addColorStop(0, 'rgb(187, 156, 255)')
+gradientB.addColorStop(1, 'rgb(164,160,253)')
+var gradientC = ctx.createLinearGradient(0, 0, 0, 600)
+gradientC.addColorStop(0, 'rgb(111,207,153)')
+gradientC.addColorStop(1, 'rgb(107,209,192)')
+
 new Chart(document.getElementById('myChart'), {
     type: 'doughnut',
     data: {
@@ -9,7 +19,7 @@ new Chart(document.getElementById('myChart'), {
         datasets: [
             {
                 label: 'Впечатления от номера',
-                backgroundColor: ['#C4C4C4', '#C4C4C4', '#3cba9f', '#e8c3b9'],
+                backgroundColor: [gradientA, gradientB, gradientC, '#e8c3b9'],
                 data: [50, 25, 25, 0],
             },
         ],
