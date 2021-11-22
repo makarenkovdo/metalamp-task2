5f526be3b925f34cda53a20d6610c3659c6047de
makarenkovdo <makarenkovdo@gmail.com>
2021-11-22 13:15:41 +0300

    make range-slider folder

---
+++ src/components/range-slider/range-slider.js
@@ -0,0 +1,5 @@
+import Slider from './../../vendor/range-slider/Slider.bundle'
+const rangeSlider = new Slider('range-slider', {
+    shouldAddBar:true, minValue: 0, maxValue:20000, step:1000
+  }
+)
