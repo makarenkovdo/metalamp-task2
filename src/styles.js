d8557a671cb65be17057a3d61e74c9b5021f7d29
makarenkovdo <makarenkovdo@gmail.com>
2021-11-24 14:15:19 +0300

    fill-in datepicker

--- src/styles.js
+++ src/styles.js
@@ -7,9 +7,6 @@ importAll(require.context('./components/', true, /\.(svg|png)$/))
 importAll(require.context('./card-blocks/', true, /\.(svg|png|jpg)$/))
 importAll(require.context('./pages/', true, /\.(svg|png|jpg)$/))
 
-// importAll(require.context('./components/', true, /\.png$/))
-// importAll(require.context('./card-blocks/', true, /\.png$/))
-
 importAll(require.context('./components/', true, /\.scss$/))
 importAll(require.context('./pages/', true, /\.(scss|css)$/))
 importAll(require.context('./styles/', true, /\.scss$/))
